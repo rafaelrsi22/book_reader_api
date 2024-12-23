ef0737def2edbe94f4beefe39854dcf82af65a99
Rafael <rafaelrsi108@gmail.com>
2024-12-23 12:32:13 -0300

    Created route params

--- src/http/HttpRequest.java
+++ src/http/HttpRequest.java
@@ -2,22 +2,29 @@ package http;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpRequest {
     public final String method;
     public final String path;
-    Map<String, String> headers;
+    public final Map<String, String> params;
+    public final Map<String, String> headers;
 
     public HttpRequest(String method, String path, Map<String, String> headers) {
         this.method = method;
         this.path = path;
         this.headers = headers;
+        this.params = new HashMap<>();
     }
 
     public static HttpRequest fromBufferReader(BufferedReader reader) throws IOException, Exception {
         String firstLine = reader.readLine();
+        System.out.println(firstLine);
 
         if (firstLine == null) {
             throw new Exception("Empty request");
@@ -34,6 +41,27 @@ public class HttpRequest {
             headers.put(header[0], header[1]);
         }
 
-        return new HttpRequest(method, path, headers);
+        HttpRequest request = new HttpRequest(method, path, headers);
+        request.parseParams(path);
+        return request;
+    }
+
+    public void parseParams(String routePath) {
+        String regex = routePath.replaceAll(":[^/]+", "([^/]+)");
+        Pattern paramPattern = Pattern.compile(regex);
+        Matcher paramMatcher = paramPattern.matcher(this.path);
+
+        if (paramMatcher.matches()) {
+            List<String> paramNames = new ArrayList<>();
+            Pattern namePattern = Pattern.compile(":[^/]+");
+            Matcher nameMatcher = namePattern.matcher(routePath);
+            while(nameMatcher.find()){
+                paramNames.add(nameMatcher.group().substring(1));
+            }
+
+            for(int i = 0; i < paramNames.size(); i++){
+                params.put(paramNames.get(i), paramMatcher.group(i+1));
+            }
+        }
     }
 }
--- src/routes/RouteConfig.java
+++ src/routes/RouteConfig.java
@@ -6,9 +6,10 @@ import models.Book;
 
 public class RouteConfig {
     public static void init() {
-        HttpController.get("/", ((request, response) -> {
-            Book myBook = BooksController.getAllBooks().get(0);
-            response.sendJson(myBook.toString());
+        HttpController.get("/:id", ((request, response) -> {
+//            Book myBook = BooksController.getAllBooks().get(0);
+//            response.sendJson(myBook.toString());
+            response.sendString(request.params.get("id"));
         }));
     }
 }
